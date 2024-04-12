350848359ccc606638141c7098e670e3b6b10519
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-04-11 22:04:53 -0500

    Agregue el codigo en la tabla de catalogos_tipo

--- database/migrations/2019_10_11_024200_create_catalogo_tipo.php
+++ database/migrations/2019_10_11_024200_create_catalogo_tipo.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('catalogo_tipo', function (Blueprint $table) {
             $table->id();
+            $table->string("codigo",5)->unique();
             $table->string('nombre');
             $table->string('descripcion');
             $table->timestamps();
